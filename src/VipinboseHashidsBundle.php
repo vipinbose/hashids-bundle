6721f4c1f6d5c0ac0c448fffc684fbd24e530b3f
Vipin Bose <vipin.bose@gov.in>
2024-06-05 17:18:00 +0530

    DI fix and cleanup - Symfony 6.4

--- src/VipinboseHashidsBundle.php
+++ src/VipinboseHashidsBundle.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Vipinbose\HashidsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Vipinbose\HashidsBundle\DependencyInjection\VipinboseHashidsExtension;
 
 class VipinboseHashidsBundle extends Bundle
 {
+    public function getContainerExtension(): VipinboseHashidsExtension
+    {
+        return new VipinboseHashidsExtension;
+    }
 }
